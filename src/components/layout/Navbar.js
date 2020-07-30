edab040c84ed947ef4e582180721fe2a3318169d
Peter Kang <peeterkang@gmail.com>
2020-07-29 17:07:46 -0700

    logos coordinate with theme

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
+import { lightTheme } from '../../theme';
 import { NavLink } from 'react-router-dom';
-// import logo from '../../assets/imgs/PK.png';
+import logo from '../../assets/imgs/PK.png';
 import logoLight from '../../assets/imgs/PK_Light_small.png';
 
 const Navbar = () => {
+  const themeContext = useContext(ThemeContext);
   return (
     <nav className='navbar'>
       <h1>
         <NavLink to='/'>
-          <img src={logoLight} alt='logo'></img>
+          {themeContext === lightTheme ? (
+            <img src={logoLight} alt='logo'></img>
+          ) : (
+            <img src={logo} alt='logo'></img>
+          )}
         </NavLink>
       </h1>
       <ul>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,19 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from '../../theme';
+import { ThemeContext } from 'styled-components';
+import { lightTheme } from '../../theme';
 import PK_WD from '../../assets/imgs/PK_WD.png';
 import PK_Light_WD from '../../assets/imgs/PK_Light_WD.png';
 
 const Home = (props) => {
+  const themeContext = useContext(ThemeContext);
+
   return (
     <Fragment>
       <div className='center-logo'>
         <Link to='/about'>
-          {props.theme === lightTheme ? (
-            <img src={PK_WD} alt='big logo' className='big-logo' />
-          ) : (
+          {themeContext === lightTheme ? (
             <img src={PK_Light_WD} alt='big logo' className='big-logo' />
+          ) : (
+            <img src={PK_WD} alt='big logo' className='big-logo' />
           )}
         </Link>
       </div>
